983828b08ccdf8a34c4bd8329a0496c59a163b60
Lars Fritsche <larsf@umich.edu>
2022-04-21 12:18:07 -0400

    Update function.R

--- function.R
+++ function.R
@@ -2,7 +2,7 @@ citeRpacks <- function(pkg_list, filename, RStudio = FALSE, Endnote = FALSE) {
 
   #ht to https://stackoverflow.com/questions/2470248/write-lines-of-text-to-a-file-in-r for sink()
   for (i in 1:length(pkg_list)) {
-      sink(file = paste(filename, ".bib", sep = ""), append = T)
+      sink(file = paste(filename, ".bib", sep = ""), append = i != 1)
       writeLines(toBibtex(citation(package = pkg_list[i])))
       sink()
   }
